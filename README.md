fdec99370b1e8ba97fc7a13a193fc569a309e077
Anna Kuzmenko <mogilevs.anna@gmail.com>
2023-06-28 23:19:25 +0300

    added todo app

--- README.md
+++ README.md
@@ -1,9 +1,33 @@
-# React TypeScript Starter Pack
+Todo App
+This is a simple Todo application that allows you to manage your tasks. You can add new tasks, delete them, mark them as completed, toggle the completion status of all tasks, and sort them.
 
-To use this template click `Use this template`
+Features
+Add Todo: Easily add new tasks to your list.
+Delete Todo: Remove tasks from your list when they are no longer needed.
+Mark as Completed: Mark tasks as completed to track your progress.
+Toggle All: Toggle the completion status of all tasks at once.
+Sort Todos: Sort tasks based on different criteria to organize your list.
+Error Handling: Display error messages if any issues occur during task management.
+Technologies Used
+React: JavaScript library for building user interfaces.
+React Loader Spinner: Component for displaying loading spinners.
+REST API: Used to interact with the backend server for CRUD operations on tasks.
+Getting Started
+Clone the repository: git clone https://github.com/your-repo.git
+Install the dependencies: npm install
+Start the development server: npm start
+Usage
+Open the Todo App in your web browser.
+Add a new task by entering the title and pressing the "Enter" key.
+Delete a task by clicking the delete icon next to it.
+Mark a task as completed by clicking the checkbox next to it.
+Toggle the completion status of all tasks by clicking the "Toggle All" button.
+Sort tasks using the available sorting options (e.g., All, Completed, Active).
+To delete all completed tasks, click the "Clear Completed" button.
+License
+This project is licensed under the MIT License.
 
-### Available Scripts
-
-`Deploy` - available to deploy your application to gh-pages
-
-`SCSS Preprocessor` - available to write your styles with modern style language
+Acknowledgements
+React documentation: https://reactjs.org/
+React Loader Spinner documentation: https://www.npmjs.com/package/react-loader-spinner
+REST API documentation: link-to-rest-api-docs
--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,242 @@
-import React from 'react';
-import './App.scss';
+import {
+  FC,
+  useState,
+  useEffect,
+  useMemo,
+  useCallback,
+} from 'react';
+import { ThreeDots } from 'react-loader-spinner';
+import {
+  getTodos,
+  deleteTodo,
+  updateTodo,
+  createTodo,
+} from './api/todos';
+import { UserWarning } from './UserWarning';
+import { Todo } from './types/Todo';
+import { Error } from './types/Errors';
+import { Header } from './components/Header';
+import { TodoList } from './components/TodoList';
+import { FilterBy } from './types/TodosFilter';
+import { Notification } from './components/Notification';
+import { Footer } from './components/Footer';
 
-interface Props {
-  onClick: () => void;
-}
+const USER_ID = 10353;
 
-export const Provider: React.FC<Props> = React.memo(
-  ({ onClick, children }) => (
-    <button
-      type="button"
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  ),
-);
+export const App: FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [filter, setFilter] = useState(FilterBy.ALL);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadingTodoIds, setLoadingTodoIds] = useState<number[]>([]);
 
-export const App: React.FC = () => {
+  const loadTodos = useCallback(async () => {
+    try {
+      const todosFromServer = await getTodos(USER_ID);
+
+      setTodos(todosFromServer);
+      setIsLoading(false);
+    } catch (error) {
+      setErrorMessage(Error.LOAD);
+    }
+
+    setIsLoading(false);
+  }, []);
+
+  const visibleTodos = useMemo(() => {
+    return todos.filter(todo => {
+      switch (filter) {
+        case FilterBy.ALL:
+          return true;
+
+        case FilterBy.COMPLETED:
+          return todo.completed;
+
+        case FilterBy.ACTIVE:
+          return !todo.completed;
+
+        default:
+          return true;
+      }
+    });
+  }, [todos, filter]);
+
+  const activeTodosLength = useMemo(() => {
+    return todos.filter((todo) => !todo.completed).length;
+  }, [todos]);
+
+  const completedTodos = useMemo(() => {
+    return todos.filter(todo => todo.completed === true);
+  }, [todos]);
+
+  const addLoadingTodoId = (todoId: number) => {
+    setLoadingTodoIds(prevIds => [...prevIds, todoId]);
+  };
+
+  const removeLoadingTodoId = (todoId: number) => {
+    setLoadingTodoIds(prevIds => prevIds.filter(id => id !== todoId));
+  };
+
+  const handleAddTodo = useCallback(async (newTodo: Todo) => {
+    setTempTodo(newTodo);
+
+    try {
+      const todoToAdd = await createTodo({
+        id: 0,
+        title: newTodo.title,
+        completed: false,
+        userId: USER_ID,
+      });
+
+      setTodos((prevTodos) => [...prevTodos, todoToAdd]);
+    } catch {
+      setErrorMessage(Error.ADD);
+    } finally {
+      setTempTodo(null);
+    }
+  }, []);
+
+  const handleDelete = useCallback(async (todoToDelete: Todo) => {
+    try {
+      addLoadingTodoId(todoToDelete.id);
+      await deleteTodo(todoToDelete.id);
+      setTodos((prevTodos) => prevTodos
+        .filter((todo) => todo.id !== todoToDelete.id));
+    } catch {
+      setErrorMessage(Error.DELETE);
+    } finally {
+      removeLoadingTodoId(todoToDelete.id);
+    }
+  }, []);
+
+  const handleDeleteCompletedTodos = useCallback(async () => {
+    try {
+      completedTodos.map(todo => handleDelete(todo));
+    } catch {
+      setErrorMessage(Error.DELETE);
+    }
+  }, [todos]);
+
+  const handleUpdateCompletedTodo = useCallback(
+    async (id: number, completed: boolean) => {
+      addLoadingTodoId(id);
+
+      const updatePromises = todos
+        .filter((todo) => todo.id === id)
+        .map((todo) => updateTodo(todo.id, { completed }));
+
+      try {
+        await Promise.all(updatePromises);
+
+        setTodos((prevTodos) => prevTodos
+          .map((todo) => (todo.id === id ? { ...todo, completed } : todo)));
+      } catch {
+        setErrorMessage(Error.UPDATE);
+      } finally {
+        removeLoadingTodoId(id);
+      }
+    }, [todos],
+  );
+
+  const handleUpdateTodoTitle = useCallback(
+    async (id: number, title: string) => {
+      addLoadingTodoId(id);
+      try {
+        await updateTodo(id, { title });
+
+        setTodos((prevTodos) => prevTodos.map((todo) => {
+          if (todo.id === id) {
+            return {
+              ...todo,
+              title,
+            };
+          }
+
+          return todo;
+        }));
+      } catch {
+        setErrorMessage(Error.UPDATE);
+      } finally {
+        removeLoadingTodoId(id);
+      }
+    }, [],
+  );
+
+  const handleToggleAll = useCallback(() => {
+    if (!activeTodosLength) {
+      todos.forEach(todo => {
+        handleUpdateCompletedTodo(todo.id, !todo.completed);
+      });
+    } else {
+      todos.forEach(todo => {
+        if (!todo.completed) {
+          handleUpdateCompletedTodo(todo.id, !todo.completed);
+        }
+      });
+    }
+  }, [todos]);
+
+  useEffect(() => {
+    loadTodos();
+  }, []);
+
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>
-        <TodoList />
-      </Provider>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      {isLoading ? (
+        <ThreeDots
+          height="80"
+          width="80"
+          radius="9"
+          color="#eb8a44"
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible
+        />
+      ) : (
+        <div className="todoapp__content">
+          <Header
+            onAddTodo={handleAddTodo}
+            onToggleAll={handleToggleAll}
+            onError={setErrorMessage}
+            activeTodosLength={activeTodosLength}
+          />
+
+          {!!todos.length && (
+            <TodoList
+              todos={visibleTodos}
+              tempTodo={tempTodo}
+              loadingTodoIds={loadingTodoIds}
+              onDelete={handleDelete}
+              onCompleteUpdate={handleUpdateCompletedTodo}
+              onTitleUpdate={handleUpdateTodoTitle}
+            />
+          )}
+
+          {!!todos.length && (
+            <Footer
+              activeTodosLength={activeTodosLength}
+              filter={filter}
+              setFilter={setFilter}
+              handleDeleteCompletedTodos={handleDeleteCompletedTodos}
+            />
+          )}
+        </div>
+      )}
+
+      {errorMessage && (
+        <Notification
+          message={errorMessage}
+          handleMessage={setErrorMessage}
+        />
+      )}
     </div>
   );
 };
